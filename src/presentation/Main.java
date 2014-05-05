a3c01b13ec5e92b2ce5717da6d2382bce7f717a7
Christian <christian@ditz64.de>
2014-05-05 12:42:21 +0200

    * Merge from feature-artikel

---
+++ src/presentation/Main.java
@@ -0,0 +1,9 @@
+package presentation;
+
+import presentation.article.Article;
+
+public class Main {
+    public static void Main(String[] args) {
+        Article article = new Article();
+    }
+}
