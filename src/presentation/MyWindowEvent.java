96be34671d81aeabb17d4f9c41f5084c8e92fcd8
Christian <christian@ditz64.de>
2014-05-05 12:12:18 +0200

    * Merge from feature-artikel

--- src/presentation/MyWindowEvent.java
+++ src/presentation/MyWindowEvent.java
@@ -1,8 +1,36 @@
 package presentation;
-import javax.swing.*;
-/**
- * Created by Marius on 05.05.14.
- */
-public class MyWindowEvent {
 
+import javax.swing.*;
+import java.awt.event.*;
+
+public class MyWindowEvent extends WindowAdapter
+        implements ActionListener
+{
+    JFrame frame = null;
+
+    public MyWindowEvent(JFrame frame){
+        this.frame = frame;
+    }
+
+    public void windowClosing (WindowEvent e) {
+        JFrame frame = null;
+        frame =(JFrame) e.getSource();
+        System.out.println("Hallo");
+        frame.dispose();
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        Object object = e.getSource();
+        JButton bu_work = null;
+
+        System.out.println("Test");
+
+        if (object instanceof JButton){
+            bu_work = (JButton) object;
+            if(bu_work.getName() == "bt_abbrechen"){
+                System.out.println("Hallo");
+                frame.dispose();
+            }
+        }
+    }
 }
