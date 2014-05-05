22f14aa8dc092499ae404e122957f44bea04270e
Marius <marius.averhoff@yahoo.de>
2014-05-05 11:59:53 +0200

    + ListenHandler + Main + Artikel Form

---
+++ src/presentation/main.java
@@ -0,0 +1,11 @@
+package presentation;
+
+/**
+ * Created by Marius on 05.05.14.
+ */
+public class Main {
+    public static void main(String[] args){
+        Artikel winArtikel = new Artikel();
+
+    }
+}
