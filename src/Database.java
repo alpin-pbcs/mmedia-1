00a946c7c88219c47a9d2a6445c04e13071bc4d0
Sebastian <sbackes@live.de>
2014-05-05 09:37:58 +0200

    * First commit

---
+++ src/Database.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Sebastian on 05.05.14.
+ */
+public class Database {
+
+
+
+}
---
+++ src/Start.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Sebastian on 05.05.14.
+ */
+public class Start {
+
+    public void main(){
+
+    }
+
+
+}
